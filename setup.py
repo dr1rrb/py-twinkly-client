82bfc5a0932192f0639ce8f75db8b39b3aeabb70
David <github.site@torick.net>
2020-10-20 21:10:18 -0400

    chore(package): Update package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="twinkly-client-dr1rrb", # Replace with your own username
+    name="twinkly-client",
     version="0.0.1",
     author="dr1rrb",
     author_email="py-twinkly-client@torick.net",
